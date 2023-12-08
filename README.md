558595f68b09c320b48274ed9bada6b3ebfca70a
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-08 10:27:42 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Slide-Bar-uchun
+ 
---
+++ js/index.js
@@ -0,0 +1,11 @@
+var toggle = document.getElementById("toggle");
+var container = document.getElementById("container");
+
+toggle.onclick = function(){
+	container.classList.toggle('active');
+}
+
+
+
+
+// Suyunov Husan
